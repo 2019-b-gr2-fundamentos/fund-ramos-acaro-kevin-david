554e89c883f6615f24af1f879b4df9df2eebfd36
Kevin Ramos <kevin.ramos01@epn.edu.ec>
2019-10-17 17:18:53 -0500

    Primitivas

--- 01-Introduccion/01-variables.js
+++ 01-Introduccion/01-variables.js
@@ -9,3 +9,14 @@ Descripcion: Ejemplos de variables primitivas en Js
 /* <- abro comentario
 
 cierro bloque comentario -> */
+
+//Primitivas (tipos de variables)
+/*
+int        -> Numero ENTERO     Ej: 5 o -3 o 0
+char       -> Caracter          Ej: a b c 
+float      -> N. decimal        Ej: 12.12 o -1.2
+double     -> N. decimal ++     Ej: 1212.......
+string     -> Arreglo de char   Ej: "abc" o "cd"
+null       -> VACIO             Ej: null
+boolean    ->V o F              Ej: true o false
+*/
