f519e3e57dc539484bfd09243a280a37b205e893
Kevin Ramos <kevin.ramos01@epn.edu.ec>
2019-10-17 17:52:48 -0500

    Fin de clase

--- 01-Introduccion/01-variables.js
+++ 01-Introduccion/01-variables.js
@@ -20,3 +20,16 @@ string     -> Arreglo de char   Ej: "abc" o "cd"
 null       -> VACIO             Ej: null
 boolean    ->V o F              Ej: true o false
 */
+// var edad = 30; // 
+let edad = 30; //Mutable
+//const edad = 30; // Inmutable
+const IVA = 12; 
+//IVA = 14; // no esta permitido, NO REASIGNAR VALORES 
+let sueldo = 0;
+// edad = 30 -> Python
+// var edad: Integer = 30; -> kotlin Mutable
+// val edad: Integer = 30; -> kotlin Inmutable
+
+// --> INMUTABLES
+//let IVA = 12;
+//const IVA = 12; 
