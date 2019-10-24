46e20ea47ded895dc0aa6a87bca89d6c0130976e
Kevin-ramos <55715422+Kevin-ramos@users.noreply.github.com>
2019-10-24 17:56:29 -0500

    Strings

--- 01-Introduccion/03-Numeros-strings.js
+++ 01-Introduccion/03-Numeros-strings.js
@@ -16,6 +16,40 @@ resultado = 4 % 2; // 0 -> Residuo
 resultado = 4 % 3; // 1 -> Residuo
 resultado = 7 % 3; // 1 -> Residuo
 console.log("pera" + 2);
+//STRINGS
+var mensaje = "\"Hola mundo\"";
+var saludo = "\'Adio\s mundo\'";
+console.log(mensaje + "1\t\t\t  3\n" + saludo);
+//template strings
+var edad = 30;
+var saludoEdad = "Mi edad es: \n" + edad;
+var saludoCompleto = mensaje + "\n" + saludo;
+console.log(saludoEdad);
+console.log(saludoCompleto);
+//Contamos los elementos
+var nombreLongitud = "Vicente Adrian";
+console.log(nombreLongitud.length);
+//Eliminar espacios al principio y al final
+console.log(nombreLongitud.trim());
+//Cambiar a mayusculas
+console.log(nombreLongitud.toUpperCase());
+console.log(nombreLongitud.toLowerCase());
+console.log(nombreLongitud);
+console.log(nombreLongitud.replace("x", "o"));
+console.log(nombreLongitud);
+console.log(nombreLongitud.replace("a", "o"));
+console.log(nombreLongitud);
+console.log(nombreLongitud.search("z")); //-1
+console.log(nombreLongitud.search("Adrian")); //
+console.log(nombreLongitud.slice(0, 6)); //
+console.log(nombreLongitud.indexOf("a")); //
+console.log(nombreLongitud.indexOf("x")); // -1
+console.log(nombreLongitud.includes(" ")); // booleano
+console.log(nombreLongitud.includes("x")); // booleano
+console.log(nombreLongitud.endsWith("x")); // booleano
+console.log(nombreLongitud.startsWith("Vice")); // booleano
+console.log(nombreLongitud.substring(0, 6)); //
+console.log(nombreLongitud.replace("\n", ""));
 //NUMEROUNO -> TODO MAYUSCULA (SEPARACION NO)
 //NUMERO_UNO -> mayusculas guion bajo
 //NumeroUno -> 
