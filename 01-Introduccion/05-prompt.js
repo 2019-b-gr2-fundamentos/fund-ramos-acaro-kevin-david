ccc6a30ed954e9a72121b65e675a24f275467f96
Kevin-ramos <55715422+Kevin-ramos@users.noreply.github.com>
2019-11-03 21:56:26 -0500

    Calculadora py

---
+++ 01-Introduccion/05-prompt.js
@@ -0,0 +1,5 @@
+var numeroUnoString = prompt("Ingresa numero 1"); //STRING
+var numeroDosString = prompt("Ingresa numero 2"); //STRING
+var numeroUno = Number(numeroUnoString);
+var numeroDos = Number(numeroDosString);
+console.log(numeroUno + numeroDos);
