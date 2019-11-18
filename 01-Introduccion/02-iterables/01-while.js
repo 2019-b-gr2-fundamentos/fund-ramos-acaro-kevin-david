2ec040be2ee4d51cc09d00bcf70d2e35125efc4b
Kevin-ramos <kevin.ramos01@epn.edu.ec>
2019-11-18 15:27:41 -0500

    Arreglos

--- 01-Introduccion/02-iterables/01-while.js
+++ 01-Introduccion/02-iterables/01-while.js
@@ -18,3 +18,79 @@ do {
             break;
     }
 } while (vecesQueHeComido < 3);
+while (vecesQueHeComido < 3) {
+    vecesQueHeComido = 0;
+    vecesQueHeComido = vecesQueHeComido + 1;
+    console.log('Vamos a comer');
+    switch (vecesQueHeComido) {
+        case 1:
+            console.log('Desayuno');
+            break;
+        case 2:
+            console.log('Almuerzo');
+            break;
+        case 3:
+            console.log('Merienda');
+            break;
+        case 4:
+            console.log('Gorditos');
+            break;
+    }
+}
+/*
+1 Analisis socioeconomico
+2 Algebra
+3 Compiladores
+5 Fundamentos de programacion
+7 Calculo
+11 Fisica
+13 Ingles
+
+si es impar -> aumentamos a analisis 17
+si es par -> aumentamos  a algebra 17
+si es multiplo de 3 -> aumentamos a compiladores 16
+
+
+*/
+var Deberes = 0;
+var Analisis = 0;
+var Algebra = 0;
+var Compiladores = 0;
+var Programacion = 0;
+var Calculo = 0;
+var Fisica = 0;
+var Ingles = 0;
+do {
+    Deberes = Deberes + 1;
+    if (Deberes % 13 == 0) {
+        Ingles = Ingles + 1;
+    }
+    else if (Deberes % 11 == 0) {
+        Fisica = Fisica + 1;
+    }
+    else if (Deberes % 7 == 0) {
+        Calculo = Calculo + 1;
+    }
+    else if (Deberes % 5 == 0) {
+        Programacion = Programacion + 1;
+    }
+    else if (Deberes % 3 == 0) {
+        Compiladores = Compiladores + 1;
+    }
+    else if (Deberes % 2 == 0) {
+        Algebra = Algebra + 1;
+    }
+    else if (Deberes % 2 !== 0) {
+        Analisis = Analisis + 1;
+    }
+    else {
+        console.log("No tiene deberes");
+    }
+} while (Deberes < 1000);
+console.log("Hice ${Ingles} deberes de ingles");
+console.log("Hice " + Fisica + " deberes de fisica");
+console.log("Hice " + Calculo + " deberes de calculo");
+console.log("Hice " + Programacion + " deberes de programacion");
+console.log("Hice " + Compiladores + " deberes de compiladores");
+console.log("Hice " + Algebra + " deberes de algebra lineal");
+console.log("Hice " + Analisis + " deberes de analisis socioeconomico");
