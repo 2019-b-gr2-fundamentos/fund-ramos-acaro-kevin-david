4d03402fdb3e6cf51b9e2474fa265dd38edb355f
Kevin-ramos <55715422+Kevin-ramos@users.noreply.github.com>
2020-01-29 15:31:02 -0500

    Deber colegios

--- Deber-colegios/colegios.ts
+++ Deber-colegios/colegios.ts
@@ -1,21 +1,24 @@
 import { datosColegios } from "./interfaces/colegios-interfaces";
 import * as prompts from 'prompts';
 
-async function main(){
-    const arregloColegios: datosColegios[]= []
-    const preguntas = [
+let id = 1;
+let colegios: datosColegios[] = [];
+
+async function crearDatosColegios(){
+   
+    const preguntas1 = [
         {
-            type: 'string',
+            type: 'text',
             name: 'nombre',
             message: 'Inserte el nombre delcolegio'
         },
         {
-            type: 'string',
+            type: 'text',
             name: 'apodo',
             message: 'Inserte el apodo de su colegio'
         },
         {
-            type: 'string',
+            type: 'text',
             name: 'sector',
             message: 'Inserte el sector del colegio '
         },
@@ -29,89 +32,131 @@ async function main(){
             name: 'numeroEstudiantes',
             message: 'Ingrese el numero de estudiantes'
         }
+    ];
+        const respuestaPreguntas = await prompts(preguntas1);
+        const nuevoRegistroColegio = {
+            Aid: id,
+            nombre: respuestaPreguntas.nombre,
+            apodo: respuestaPreguntas.apodo,
+            sector: respuestaPreguntas.sector,
+            fundacion: respuestaPreguntas.fundacion,
+            numeroEstudiantes: respuestaPreguntas.numeroEstudiantes,
+    };
+    id = id + 1;
+    colegios.push(nuevoRegistroColegio);
+    posibilidades().then().catch();
 
-    ] 
-    async function agregarColegios(){
-        let cole: datosColegios = await prompts(preguntas)
-        arregloColegios.push(cole)
-        decidir()
-    }
-    async function editarColegios(){
-        let indice = await prompts({
-            type: 'number',
-            name: 'indice',
-            message: 'Inserte el indice del colegio que quiera editar'
-        })
-        if (indice.indice < arregloColegios.length){
-            let colegioEditado = await prompts({
-                type: 'text',
-                name: 'caracteristica',
-                message:'¿Qué es lo que desea editar?'
-            })
-            if (colegioEditado.caracteristica == 'nombre'
-            || colegioEditado.caracteristica == 'apodo'
-            || colegioEditado.caracteristica == 'sector'
-            || colegioEditado.caracteristica == 'fundacion'
-            || colegioEditado.caracteristica == 'numeroEstudiantes'){
-                let nuevovalor = await prompts({
-                    type: 'text',
-                    name: 'valor',
-                    message: '¿Que desea insertar?'                                        
-                })
-                let colegioSeleccionado = arregloColegios[indice.indice]
-                switch (colegioEditado.caracteristica){
-                    case 'Nombre':
-                        colegioSeleccionado.nombre = nuevovalor.valor
-                        break;
-                    case 'Pais':
-                        colegioSeleccionado.apodo = nuevovalor.valor
-                        break;
-                    case 'Liga':
-                        colegioSeleccionado.sector = nuevovalor.valor
-                        break;
-                    case 'Presupuesto':
-                        colegioSeleccionado.fundacion = nuevovalor.valor
-                        break;
-                    case 'Presupuesto':
-                        colegioSeleccionado.numeroEstudiantes = nuevovalor.valor
-                        break;           
+};
+async function posibilidades(){
+    const preguntas = await prompts({
+        type: 'text',
+        name: 'respuestas',
+        message: '¿Que quieres hacer? \n 1-Crear un colegio \n 2-Ver los colegios  \n 3-Actualizar colegios \n 4-Eliminar colegios \n 5-Salir'
+    });
+    const respuesta1 = preguntas.respuestas;
+    if(respuesta1 == 1){
+        crearDatosColegios().then().catch();
+        
+    }else if(respuesta1 == 2){
+        leerRegistros().then().catch();
+
+    }else if(respuesta1 == 3){
+        editarColegio().then().catch();
+
+    }else if(respuesta1 == 4){
+        eliminarRegistro().then().catch();
+
+    }else if(respuesta1 == 5){
+        console.log('ADIOS');
+    }else{
+        console.log('Elija una opcion valida');
+        posibilidades().then().catch();
+    };
+    return preguntas.respuestas;
+};
+async function leerRegistros(){
+    console.log('Los colegios son:', colegios);
+    posibilidades().then().catch();
+};
+async function editarColegio(){
+    const AidAEditar = await prompts({
+        type: 'number',
+        name: 'Aid',
+        message: 'Ingresa el aid del colegio disponible'
+    });
+    const AidEncontrado = colegios.findIndex(
+        function(valorActual){
+        return valorActual.Aid == AidAEditar.Aid
         }
-        decidir()
-            }
-            else{
-                console.log('la edicion no existe')
-                editarColegios()
-            }
-        }
-        else{
-            console.log('No existe el indice')
-            editarColegios()
-        }    
-    }
-    async function decidir(){
-        let decision = await prompts({
+    );
+    const colegioAeditar = await prompts({
+        type: 'text',
+        name: 'campoAEditar',
+        message: '¿Que colegio desea editar?'
+    });
+    const respuestaCampo = colegioAeditar.campoAEditar;
+    if(respuestaCampo == 'nombre'){
+        const nuevoColegio = await prompts({
             type: 'text',
-            name: 'eleccion',
-            message: 'Insertar nuevo colegio --> 1 \n Editar colegio --> 2 \n Salir --> 3'
-        })
-        switch(decision.eleccion){    
-            case '1':
-                agregarColegios()
-                break;
-            case '2':
-                editarColegios()
-                break;
-            case '3':
-                console.log('Estos son tus colegios:')
-                console.log(arregloColegios)  
-                break;
-            default:
-                console.log('La opción elegida no es válida')
-                decidir() 
-                break;
+            name: 'nuevoColegio',
+            message: 'Ingrese el nuevo colegio'
+        });
+        colegios[AidEncontrado].nombre = nuevoColegio.nuevoColegio;
+    }else if(respuestaCampo == 'apodo'){
+        const nuevoApodo = await prompts({
+            type: 'text',
+            name: 'nuevoApodo',
+            message: 'Ingrese el nuevo apodo del colegio'
+        });
+        colegios[AidEncontrado].apodo = nuevoApodo.nuevoApodo;
+    }else if(respuestaCampo == 'sector'){
+        const nuevoSector = await prompts({
+            type: 'text',
+            name: 'nuevoSector',
+            message: 'Ingrese el nuevo '
+        });
+        colegios[AidEncontrado].sector = nuevoSector.nuevoSector;
+    }else if(respuestaCampo == 'fundacion'){
+        const nuevaFundacion = await prompts({
+            type: 'text',
+            name: 'nuevaFundacion',
+            message: 'Ingrese la nueva fundacion del colegio'
+        });
+        colegios[AidEncontrado].fundacion = nuevaFundacion.nuevaFundacion;
+    }else if(respuestaCampo == 'numeroEstudiantes'){
+        const nuevosEstudiantes = await prompts({
+            type: 'text',
+            name: 'estudiantes',
+            message: 'Ingrese el nuevo numero de estudiantes'
+        });
+        colegios[AidEncontrado].numeroEstudiantes = nuevosEstudiantes.estudiantes;
+    }else{
+        console.log('Ingrese una opcion valida');
+    };
+    console.log('El registro de colegios actualizado es:', colegios);
+    posibilidades().then().catch();
+    return colegios
+};
+async function eliminarRegistro(){
+    const AidAEliminar = await prompts({
+        type: 'number',
+        name: 'Aid',
+        message: 'Ingrese el Aid del colegio  que quiere eliminar'
+    });
+    const AidEncontrado = colegios.findIndex(
+        function(valorActual){
+        return valorActual.Aid == AidAEliminar.Aid
         }
-    }
-    agregarColegios()    
-    
-    }
-main()
+    );
+    colegios.splice(AidEncontrado, 1);
+    console.log('El nuevo registro de colegios es:', colegios);
+    posibilidades().then().catch();
+    return colegios
+}
+
+function main(){
+    crearDatosColegios().then().catch();
+
+}
+
+main();
--- Deber-colegios/interfaces/colegios-interfaces.ts
+++ Deber-colegios/interfaces/colegios-interfaces.ts
@@ -1,4 +1,5 @@
 export interface datosColegios{
+    Aid: number,
     nombre: string;
     apodo: string;
     sector: string;
